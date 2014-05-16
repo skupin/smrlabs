121c82fcf26b8fc56044bf58747aa9dc8e6c0eb7
Max Skupin <maxskupin@gmail.com>
2014-05-16 02:39:03 -0400

    reverted to functional

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smrlabs::Application.config.secret_key_base = '9dc2c41dd928e9ea6cb23170333a49e0687eb61a1a21bf90f2726929c09ce937643c7aaa935e9f2add9d26cc5e6afebe2a9c5237490b0aabb4f397de60f46c6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smrlabs::Application.config.session_store :cookie_store, key: '_smrlabs_session'
